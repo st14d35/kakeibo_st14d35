92fec053862af33bd936528a016fcdec39f49dc9
Junnosuke Houshido <st14d35@gm.ibaraki-ct.ac.jp>
2016-10-13 15:11:40 +0900

    main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define N 5
+
+int main(void)
+{
+  int i;
+  int payments[N];
+  
+  for(i = 0; i < N; i++){
+     printf(" payments[%d] = ? > ", i);
+      scanf("%d", &payments[i]);
+  }
+
+  return(0);
+}
