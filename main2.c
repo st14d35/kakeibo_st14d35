6eac0e9407d3f8900bcb48a441c077b94b180c15
Junnosuke Houshido <st14d35@gm.ibaraki-ct.ac.jp>
2016-10-20 15:39:18 +0900

    main2.c prototype2.

--- main2.c
+++ main2.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+#include "write_file.h"
 
 int main(int argc, char *argv[])
 {
   if(argc == 0){ printf("Error\n"); return(-1); }
 
   int incomes[256], payments[256];
-  int finish, i = 1;
+  int finish;
+  int count = 0, i = 1;
 
   while(1){
     printf("Input your income No.%d > ", i);
      scanf("%d", &incomes[i-1]);
+
+    count++;
 
     printf("If you finish to input incomes, input '0'.\n");
     printf("Otherwise it input other number.\n");
@@ -21,11 +25,19 @@ int main(int argc, char *argv[])
     i++;
   }
 
-  i = 1, finish = 1; 
+  int j, total_in = 0;
+
+  for(j = 0; j < count; j++){
+     total_in = total_in + incomes[j];
+  } 
+
+  i = 1, finish = 1, count = 0; 
 
   while(1){
     printf("Input your payment No.%d > ", i);
      scanf("%d", &payments[i-1]);
+
+    count++;
 
     printf("If you finish to input incomes, input '0'.\n");
     printf("Otherwise it input other number.\n");
@@ -37,7 +49,13 @@ int main(int argc, char *argv[])
     i++;
   }
   
-  write_file(argv[0], incomes, payments);
+  int total_out = 0;
+
+  for(j = 0; j < count; j++){
+     total_out = total_out + payments[j];
+  } 
+
+  write_file(argv[0], incomes, payments, total_in, total_out);
 
   printf("Finish.\n");
  
