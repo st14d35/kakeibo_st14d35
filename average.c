1d1a5ee5800f11d0f6c604a11ea16f83fb536186
NAGATA RYOUSUKE <st14d29@gm.ibaraki-ct.ac.jp>
2016-10-20 14:32:27 +0900

    arange修正後

--- average.c
+++ average.c
@@ -6,12 +6,12 @@ int average(int payments[]);
 
 int average(int payments[])
 {
-    int i,x; 
-    x = 0;
-   for(i = 0;i < 5;i++){
-       x = x + payments[i];
-      }
-   x = x / 5;
-return x;
+ int i,x; 
+ x = 0;
+ for(i = 0;i < 5;i++){
+    x = x + payments[i];
+    }
+ x = x / 5;
+ return x;
 }
 
